83fbb1a74ec3422c9f159def6db5080ec009f487
Leo Taylor <leotaylor723@gmail.com>
2018-06-27 11:33:31 -0500

    get listings array

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -4,9 +4,19 @@ import './Listings.css';
 
 class Listings extends React.Component {
   render () {
+    const {listings} = this.props;
+    // const listings = this.props.listings; same as above
+    const listingsItemComponents = listings.map((item) => {
+      return (
+        <li key={item.id}>{item.price}</li>
+      );
+    });
     return (
       <div className="Listings">
         <h2>Listings</h2>
+        <ul>
+          {listingsItemComponents}
+        </ul>
       </div>
     );
   }
